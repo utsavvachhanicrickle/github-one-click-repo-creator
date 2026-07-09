5b005ba18710141d4457abe90196110598249e39
utsavvachhanicrickle <utsavvachhani.cs@gmail.com>
2026-07-09 14:37:44 +0530

    0907202606 - deleted somthing

--- github-one-click-repo-creator/backend/src/middleware/auth.middleware.js
+++
@@ -1,6 +0,0 @@
-export function requireGithubLogin(req, res, next) {
-  if (!req.session.githubAccessToken || !req.session.githubUser) {
-    return res.status(401).json({ message: 'Please login with GitHub first.' });
-  }
-  next();
-}
